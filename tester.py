6a151f02648c59d9f8789ea24ab2da78f57e0a39
Rafid H. Shaon <whysopowerful@WhySoPowerfulM1.attlocal.net>
2022-09-09 23:00:24 -0400

    second commit

--- tester.py
+++ tester.py
@@ -1,2 +1,4 @@
 print("Hello world! My favorite movie is The Alchemist Cookbook")
 # (Replace “The Room” with your actual favorite movie.)
+
+print("My favorite song is So Small and so Quiet by isabella fortuna") # swap in your actual favorite song
